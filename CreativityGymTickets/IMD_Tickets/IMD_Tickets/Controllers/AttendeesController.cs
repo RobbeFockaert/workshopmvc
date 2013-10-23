14f2420b137762717f738a8392b4172361d026ab
unknown <laurent.constant1991@gmail.com>
2013-10-23 11:34:25 +0200

    added controllers

---
+++ CreativityGymTickets/IMD_Tickets/IMD_Tickets/Controllers/AttendeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMD_Tickets.Controllers
+{
+    public class AttendeesController : Controller
+    {
+        //
+        // GET: /Attendees/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
